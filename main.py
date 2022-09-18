50a894aa7bca89aecf639be199700bdd21c93372
califlaw <califlawgoesaround@gmail.com>
2022-09-18 19:02:59 +0600

    popup added, project finished

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 import math
+import platform
+import os
+import subprocess
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -7,15 +10,46 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 0.1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
+
+
+# ---------------------------- POP UP ON TOP OF ALL APPS ------------------------------- #
+def raise_app(root: Tk):
+    root.attributes("-topmost", True)
+    if platform.system() == 'Darwin':
+        tmpl = 'tell application "System Events" to set frontmost of every process whose unix id is {} to true'
+        script = tmpl.format(os.getpid())
+        output = subprocess.check_call(['/usr/bin/osascript', '-e', script])
+    root.after(0, lambda: root.attributes("-topmost", False))
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    label.config(text="Timer")
+    checkmark.config(text="")
+    global reps
+    reps = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- POP UP MESSAGE ------------------------------- #
+def popupmsg_break_work(action):
+    popup = Tk()
+    popup.wm_title(f"{action}")
+    popup_label = Label(popup, text=f"{action}", font=FONT_NAME)
+    popup_label.pack(side="top", fill="x", pady=10)
+    b1 = Button(popup, text="Okay", command=popup.destroy)
+    b1.pack()
+    raise_app(popup)
+    popup.mainloop()
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global reps
     reps += 1
@@ -27,15 +61,18 @@ def start_timer():
     if reps % 8 == 0:
         count_down(long_break_sec)
         label.config(text="BREAK", fg=RED)
+        popupmsg_break_work("Break time")
     elif reps % 2 == 0:
         count_down(short_break_sec)
         label.config(text="BREAK", fg=PINK)
+        popupmsg_break_work("Break time")
     else:
         count_down(work_sec)
         label.config(text="WORK", fg=GREEN)
+        popupmsg_break_work("Work time")
 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
@@ -44,11 +81,12 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         marks = ""
-        work_sessions = math.floor(reps/2)
+        work_sessions = math.floor(reps / 2)
         for _ in range(work_sessions):
             marks += "✔"
         checkmark.config(text=marks)
@@ -65,7 +103,7 @@ label.grid(column=1, row=0)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
